11d91ed5ffe569f82f6741ffd1f8a45234fa867d
Hamiat <hamiat.n@hotmail.com>
2025-06-21 12:51:10 +0200

    Added some db queries

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,15 @@
-from data.queries import get_all_person_table
+from data.queries import (get_all_names, 
+                          get_all_person_table, 
+                          get_all_certificates_and_names, 
+                          get_average_age,
+                          join_tables)
 
 def main():
-    get_all_person_table()
+    #get_all_person_table()
+    #get_all_names()
+    #get_all_certificates_and_names()
+    get_average_age()
+    join_tables()
 
 if __name__ == "__main__":
     main()
--- src/data/queries.py
+++ src/data/queries.py
@@ -2,9 +2,18 @@ import psycopg2
 from .config import config
 
 def get_all_person_table():
-    create_connection(query='SELECT * FROM person;')
+    db_connection(query="SELECT * FROM person;")
 
-def create_connection(query):
+def get_all_names():
+    db_connection(query="SELECT name from person")
+
+def get_all_certificates_and_names():
+    db_connection(query="SELECT name, * from certificates")
+
+def join_tables_for_person_with_scrum_certificates():
+    db_connection(query="select * from person p inner join certificates c on p.id = c.person_id where c.name = 'Scrum'")
+
+def db_connection(query, single_query=False):
     con = None
     try:
         con = psycopg2.connect(**config())
@@ -13,10 +22,29 @@ def create_connection(query):
         cursor.execute(SQL)
         for row in cursor:
             print(row)
-        cursor.close()
     except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+        print("Database error:", error)
+        return None
     finally:
+        if cursor is not None:
+            cursor.close()
         if con is not None:
             con.close()    
 
+def get_average_age():
+    con = None
+    try:
+        con = psycopg2.connect(**config())
+        cursor = con.cursor()
+        SQL = 'SELECT AVG(age) FROM person;'
+        cursor.execute(SQL)
+        row = cursor.fetchone()
+        print(f'The average age is: {row[0]:.2f}')
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Database error:", error)
+        return None
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if con is not None:
+            con.close() 
